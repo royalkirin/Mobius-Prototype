ba90a14076d6b1b149ce9b980f83b2bb780d91dd
Jacob Walston <JBwalston@student.fullsail.edu>
2021-10-29 13:58:14 -0400

    added a speed toggle

--- Assets/Scripts/lightCycle.cs
+++ Assets/Scripts/lightCycle.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 public class lightCycle : MonoBehaviour
 {
     public bool active;
+    public float speed;
 
     // Update is called once per frame
     void Update()
     {
         if(active)
         {
-            transform.Rotate(Vector3.forward, Time.deltaTime);
+            transform.Rotate(Vector3.right, speed * Time.deltaTime);
         }
     }
 }
