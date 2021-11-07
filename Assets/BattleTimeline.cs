5e29b72927f9f6fa0f6cf10e1ea422c48b53e1cd
Jacob Walston <JBwalston@student.fullsail.edu>
2021-11-07 13:20:11 -0500

    Working on card battle system

---
+++ Assets/BattleTimeline.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BattleTimeline : MonoBehaviour, IDropHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        eventData.
+    }
+}
