9e691e896b1e9481a8f5897778d82c54c2f8c188
Huy Tran <tranhonghuy95@gmail.com>
2021-11-17 13:41:22 -0500

    Card snap onto the battleground
    When played, the card will snap onto the middle of battleground. Add some basic images for visualization.

--- Assets/Scripts/Huy Combat Scripts/Cards/Card.cs
+++ Assets/Scripts/Huy Combat Scripts/Cards/Card.cs
@@ -1,24 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-
-//This class represents a Card on the Canvas
+//This class represents a Card on the Canvas that player can drag/drop/ to play
 //For now, it's base class for AttackCard, DefenseCard
 //In the future, we expand the base class depends on the need (Spell cards, sounds, animations...)
 
 public class Card : MonoBehaviour
 {
     public bool isPlayed = false;
-    [SerializeField] GameObject CardBodyPrefab;//the Body of this card, used to put them on the battleground
+    Image uiImage = null;
+
+    //images of the card. We pass these values when the cards are played onto BGCardPlayCanvas
+    [SerializeField] Sprite frontImage = null;
+    [SerializeField] Sprite backImage = null;
+    
+    private void Start()
+    {
+        FindVariables();
+    }
+
+    private void FindVariables()
+    {
+        if(frontImage is null)
+        {
+            Debug.Log("Missing front image in: " + name);
+        }
+        if(backImage is null)
+        {
+            Debug.Log("Missing back image in: " + name);
+        }
+
+        uiImage = GetComponent<Image>();
+        if(uiImage is null)
+        {
+            Debug.Log("No Image component in" + name);
+        }
+        else
+        {
+            //show the image of card front
+            uiImage.sprite = frontImage;
+        }
+    }
 
 
-    //in the future, add fancy stuffs that relates to whenever a Card is Played here.
-    //Sound effects, animation etc.
+
+
     public void Play(GameObject target)
     {
         isPlayed = true;
+        
+
         //Debug.Log("Card played in base class");//for reference
     }
 
+    public Sprite GetFrontImage()
+    {
+        return frontImage;
+    }
+
+    public Sprite GetBackImage()
+    {
+        return backImage;
+    }
+        
 }
