2e87e3d37d71a52086f6fe7bfb336a26af74f8c2
JorRDouglas <JRDouglas1@student.fullsail.edu>
2022-02-04 18:08:14 -0600

    Work on the Side Scroll Cam
    Worked on the Side Scrolling cam in a new isolated test scene labeled
    test_scene_sidescroll_cam
    This is currently unfinished and the Player Model no Cam prefab should NOT be used as of this commit.

--- Assets/Scripts/JordanD_Scripts/World Scripts/SideScrollCamera.cs
+++ Assets/Scripts/JordanD_Scripts/World Scripts/SideScrollCamera.cs
@@ -3,31 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 
 ///*****************************************************************************************///
-/// Class: SideScrollCamera                                                                  ///
+/// Class: SideScrollCamera                                                                 ///
 ///                                                                                         ///
-/// Description: 
+/// Description: This is to keep the Camera in check and ensure it follows the Player       ///
+///             properly and smoothly.                                                      ///
 ///                                                                                         ///
-///     Date Created: 1/26/22                                                                ///
-///     Date Updated: 1/26/22                                                               ///
+///     Date Created: 1/26/22                                                               ///
+///     Date Updated: 2/02/22                                                               ///
 ///                                                                                         ///
 ///     Author: Jordan R. Douglas                                                           ///
 ///*****************************************************************************************///
 public class SideScrollCamera : MonoBehaviour
 {
+    #region VARIABLES
+    //Variables
+
+    //Unity Variables
+    public Transform uPlayerTransform;
+    [SerializeField] Quaternion PlayerRotate;
+    [SerializeField] Vector3 PlayerPos;
+    Vector3 uCamDistance = new Vector3(0.0f, 0.0f, -5.0f);
+    [SerializeField] Quaternion uDebug;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FollowPlayer();
     }
 
-    void CameraRotationFix()
+    ///*********************************************************************///
+    /// Function: FollowPlayer                                              ///
+    ///                                                                     ///
+    /// Description: Keeps the camera looking at the Player as smoothly as  ///
+    ///             possible no matter where they walk on the Mobius.       ///
+    ///                                                                     ///
+    ///     Date Created: 1/31/21                                           ///
+    ///     Date Updated: 2/04/22                                           ///
+    ///                                                                     ///
+    ///     Author: Jordan R. Douglas                                       ///
+    ///*********************************************************************///
+    void FollowPlayer()
     {
-        //Code that ensures the camera gives the immersion of the worldspace not twisting and turning when the player explores.
+        //Vector3 newthing = new Vector3(Mathf.Cos(uPlayerTransform.position.x) + uCamDistance.x, uCamDistance.y, Mathf.Sin(uPlayerTransform.position.z) + uCamDistance.z);
+        //Vector3 newthing = new Vector3(Mathf.Cos(uPlayerTransform.position.x) + uCamDistance.x, uCamDistance.y, uCamDistance.z);
+
+        transform.position = uPlayerTransform.position * -1.0f;
+
+        /*transform.position = new Vector3(uPlayerTransform.position.x - uCamDistance.x, uCamDistance.y, uPlayerTransform.position.z - uCamDistance.z);
+        transform.position = uPlayerTransform.position - uCamDistance;*/
+
+        //Keeps eyes on Players
+        transform.LookAt(uPlayerTransform.position);
+
+        PlayerPos = uPlayerTransform.position;
+        uDebug = transform.rotation;
     }
 }
