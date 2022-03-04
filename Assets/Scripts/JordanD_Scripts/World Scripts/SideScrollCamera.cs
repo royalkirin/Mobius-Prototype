9160a6d6ace61264324b3118774ca9157b2ac97d
JorRDouglas <JRDouglas1@student.fullsail.edu>
2022-03-04 00:19:34 -0600

    Side Cam stuff
    Ignore this commit, it is unfinished (does not crash, but it isn't a complete version).

--- Assets/Scripts/JordanD_Scripts/World Scripts/SideScrollCamera.cs
+++ Assets/Scripts/JordanD_Scripts/World Scripts/SideScrollCamera.cs
@@ -20,10 +20,9 @@ public class SideScrollCamera : MonoBehaviour
 
     //Unity Variables
     public Transform uPlayerTransform;
+    [SerializeField] Transform uCameraFollowPoint;
     [SerializeField] Quaternion PlayerRotate;
-    [SerializeField] Vector3 PlayerPos;
-    Vector3 uCamDistance = new Vector3(0.0f, 0.0f, -5.0f);
-    [SerializeField] Quaternion uDebug;
+    Vector3 uCamDistance = new Vector3(2.0f, -2.0f, 2.0f);
     #endregion
 
     // Start is called before the first frame update
@@ -43,26 +42,30 @@ public class SideScrollCamera : MonoBehaviour
     /// Description: Keeps the camera looking at the Player as smoothly as  ///
     ///             possible no matter where they walk on the Mobius.       ///
     ///                                                                     ///
-    ///     Date Created: 1/31/21                                           ///
-    ///     Date Updated: 2/04/22                                           ///
+    ///     Date Created: 1/31/22                                           ///
+    ///     Date Updated: 2/27/22                                           ///
     ///                                                                     ///
     ///     Author: Jordan R. Douglas                                       ///
     ///*********************************************************************///
     void FollowPlayer()
     {
-        Vector3 newthing = new Vector3(Mathf.Cos(uPlayerTransform.position.x) + uCamDistance.x, uCamDistance.y, Mathf.Sin(uPlayerTransform.position.z) + uCamDistance.z);
+        //Vector3 newthing = new Vector3(Mathf.Cos(uPlayerTransform.position.x) + uCamDistance.x, uCamDistance.y, Mathf.Sin(uPlayerTransform.position.z) + uCamDistance.z);
         //Vector3 newthing = new Vector3(Mathf.Cos(uPlayerTransform.position.x) + uCamDistance.x, uCamDistance.y, uCamDistance.z);
 
-        transform.position = uPlayerTransform.position + newthing;
+        //transform.position = uPlayerTransform.position + newthing;
         //transform.position = uPlayerTransform.position * -1.0f;
 
         /*transform.position = new Vector3(uPlayerTransform.position.x - uCamDistance.x, uCamDistance.y, uPlayerTransform.position.z - uCamDistance.z);
         transform.position = uPlayerTransform.position - uCamDistance;*/
 
         //Keeps eyes on Players
-        transform.LookAt(uPlayerTransform.position);
+        //tempContainer = uPlayerTransform.transform.position + Quaternion.AngleAxis(Camera.main.transform.eulerAngles.x, Vector3.up) * Vector3.forward * -5.0f;  
+        //transform.position = tempContainer;
 
-        PlayerPos = uPlayerTransform.position;
-        uDebug = transform.rotation;
+        transform.position = uPlayerTransform.position + uCamDistance;
+        Vector3 uCross = Vector3.Cross((uPlayerTransform.position - transform.position), (uCamDistance - transform.position));
+        Quaternion uCamTilt = Quaternion.FromToRotation(transform.up, uCross);
+        transform.rotation = uCamTilt * transform.rotation;
+        //transform.LookAt(uPlayerTransform.position);
     }
 }
