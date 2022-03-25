c6f691b79d7ff53cd52c0767f76ebc6a77e9aec8
GhostyTheGamer <97315501+GhostyTheGamer@users.noreply.github.com>
2022-03-24 19:10:28 -0500

    Added a simple level select info box.
    This was done in a copy of the Main Menu. It shows up when you hover over the correct buttons.
    
    Bonus note: I re-organized my scripts folders so that it is a bit neater and easier to find.

---
+++ Assets/Scripts/SarahF_Scripts/Scripts 02/HoverOverMe.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverOverMe : MonoBehaviour
+{
+
+    [SerializeField] GameObject myPartner;
+    [SerializeField] GameObject myPartnerText;
+
+    [SerializeField] string[] infoText;
+    [SerializeField] int textUsed;
+
+    [SerializeField] GameObject levelSelect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (textUsed > infoText.Length - 1)
+        {
+            Debug.LogWarning("Selected textUsed is too high.");
+        }
+        else if (textUsed < 0)
+        {
+            Debug.LogWarning("Selected textUsed is too low.");
+        }
+        else
+        {
+            myPartnerText.GetComponent<Text>().text = infoText[textUsed];
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levelSelect.active == true)
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+    private void OnMouseOver()
+    {   
+        myPartner.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        myPartner.SetActive(false);
+    }
+}
