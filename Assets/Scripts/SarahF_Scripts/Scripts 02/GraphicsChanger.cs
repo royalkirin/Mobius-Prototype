249d951364c5d3d26cfed08e48ba361522155c19
GhostyTheGamer <97315501+GhostyTheGamer@users.noreply.github.com>
2022-03-25 19:48:58 -0500

    Added the script for the Screen Res changer.
    Added the script for the Screen Resolution changer.
    I explained how it works to the teammate I asked to implement it.

---
+++ Assets/Scripts/SarahF_Scripts/Scripts 02/GraphicsChanger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphicsChanger : MonoBehaviour
+{
+
+    [SerializeField] int selectedScreenRes;
+
+    [SerializeField] int[] screenResX;
+    [SerializeField] int[] screenResY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Screen.SetResolution(screenResX[selectedScreenRes], screenResY[selectedScreenRes], false);
+
+        gameObject.GetComponentInChildren<Text>().text = screenResX[selectedScreenRes]+" x "+screenResY[selectedScreenRes];
+    }
+
+
+    public void ChangeScreenRes()
+    {   
+    selectedScreenRes++;
+        if (selectedScreenRes >= screenResY.Length)
+        {
+            selectedScreenRes = 0;
+        }
+        else
+        {
+            
+        }
+    }
+}
