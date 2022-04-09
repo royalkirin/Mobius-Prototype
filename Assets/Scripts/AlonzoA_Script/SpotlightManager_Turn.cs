6238e150f664b3830dc3f15b1d77d0ce82b03be2
inep-t <alonzo.ayres@gmail.com>
2022-04-09 16:11:19 -0400

    Updates/Improvements for Bugs
    Script: SpotlightManager_Turn
    - Fixed errors in scene caused by script SpotlightManager_Turn not having scripts assigned.
    - Added the ability for it to do the assigning of references itself.
    - Added comments to the script to explain everything going on in the script.
    
    Levels: Tutorial2
    - Messed around with assigning references to the script SpotlightManager_Turn. (References now assign themselves. )
    - Added a toggle to the tutorial, so the player can click through the tutorial fast if they wish.
    - Adjusted the locations of various gameobjects they are in better spots.
    
    Script: TextUpdates
    - Commented on many of the functions in the script.
    - Made some updates to the code and removed some useless code that was no longer used.
    - Added a delay to the clicks, so the tutorial can't be completely rushed through.
    - Added a function to allow the tutorial to be clicked through as fast as the player can. (Toggled via CheckBox).
    - The delay function can be resumed by unclicking the checkbox.
    
    Script: Pause_Tutorial_Interaction
    - Made a new script to pause the tutorial when the pause menu is open.
    - Added to the TutorialText gameobject.
    - Script pauses the progress of the tutorial when the gear icon is hit to open the pause menu.
    - Resumes when resume is clicked.

--- Assets/Scripts/AlonzoA_Script/SpotlightManager_Turn.cs
+++ Assets/Scripts/AlonzoA_Script/SpotlightManager_Turn.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script is responable for changing the spotlights that appear over the characters once it is their turn.
+/// <summary>
+/// This script is responable for changing the spotlights that appear over the characters once it is their turn.
+//// The script works by getting the parent game objects, then finding their children, in this case the spotlights, and sets them to either true or false so they switch on or off. 
+/// </summary>
 public class SpotlightManager_Turn : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] GameObject playerLight;
-    [SerializeField] GameObject enemyLight;
+    [SerializeField] GameObject playerRef;
+    [SerializeField] GameObject enemyRef;
     [SerializeField] TurnManager turnManager;
 
     private void Start()
     {
+        playerRef = GameObject.Find("Friendly Char");
+        enemyRef = GameObject.Find("Enemy char");
         turnManager = this.GetComponent<TurnManager>();
-        playerLight.SetActive(true);
+        playerRef.transform.GetChild(5).gameObject.SetActive(true);
     }
 
     private void Update()
@@ -20,17 +27,18 @@ public class SpotlightManager_Turn : MonoBehaviour
         CurrentPlayerTurn();
     }
 
+    //This code determines what turn it is based on the isPlayerReactTurn in the Turn Manager script.
     private void CurrentPlayerTurn()
     {
-        if(turnManager.isPlayerReactTurn == true)
+        if (turnManager.isPlayerReactTurn == true)
         {
-            enemyLight.SetActive(false);
-            playerLight.SetActive(true);
+            enemyRef.transform.GetChild(6).gameObject.SetActive(false);
+            playerRef.transform.GetChild(5).gameObject.SetActive(true);
         }
-        else if(turnManager.isPlayerReactTurn == false)
+        else if (turnManager.isPlayerReactTurn == false)
         {
-            playerLight.SetActive(false);
-            enemyLight.SetActive(true);
+            playerRef.transform.GetChild(5).gameObject.SetActive(false);
+            enemyRef.transform.GetChild(6).gameObject.SetActive(true);
         }
     }
 }
