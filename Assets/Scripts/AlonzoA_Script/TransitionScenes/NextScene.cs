44199aec51095401ad57949573cffae3021fd5e3
inep-t <alonzo.ayres@gmail.com>
2022-04-30 18:41:55 -0400

    Audio & Fixes
    - Added new audio to the game for level one and two.
    - Added them to the audio manager prefab and adjusted volumes.
    - Created new scripts to call the songs to play.
    - Added code in the music scripts to call so they can stop playing the music.
    - Added code two of the scripts for going to the next screen on enemy death and to the use lose screen on player death. The code will stop the song playing for that specific level.
    - Added a new script that makes a music bool to be used in the two scripts referenced right before this.
    - Fixed issues when setting references in the scripts.

--- Assets/Scripts/AlonzoA_Script/TransitionScenes/NextScene.cs
+++ Assets/Scripts/AlonzoA_Script/TransitionScenes/NextScene.cs
@@ -5,13 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class NextScene : MonoBehaviour
 {
-    [SerializeField ] Health healthRef;
+    [SerializeField] Health healthRef;
+    TutorialMusic tutMusic = null;
+    LevelOneMusic loMusic = null;
+    LevelTwoMusic ltMusic = null;
+
     [SerializeField] bool enableKill = false;
+    [SerializeField] MusicBools[] bools;
 
     // Start is called before the first frame update
     void Start()
     {
         healthRef = this.GetComponent<Health>();
+
+        if(bools[0].isTrue == true)
+        {
+            tutMusic = FindObjectOfType<TutorialMusic>();
+        }
+        else if (bools[1].isTrue == true)
+        {
+            loMusic = FindObjectOfType<LevelOneMusic>();
+        }
+        else if (bools[2].isTrue == true)
+        {
+            ltMusic = FindObjectOfType<LevelTwoMusic>();
+        }
+
     }
 
     // Update is called once per frame
@@ -30,6 +49,7 @@ public class NextScene : MonoBehaviour
         if(healthRef.health <= 0)
         {
             Debug.Log("You Win!");
+            MusicStop();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
@@ -52,6 +72,26 @@ public class NextScene : MonoBehaviour
         {
             Debug.LogWarning("Press 'k' to test next SceneLoad.");
             i = true;
+        }
+    }
+
+    //--------------------------------------
+    /// This portion controls audio.
+    /// 
+    
+    private void MusicStop()
+    {
+        if (bools[0].isTrue == true)
+        {
+            tutMusic.Stop();
+        }
+        else if(bools[1].isTrue == true)
+        {
+            loMusic.Stop();
+        }
+        else if (bools[2].isTrue == true)
+        {
+            ltMusic.Stop();
         }
     }
 }
